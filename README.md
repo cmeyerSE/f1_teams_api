a818d36f74527c29b70840d3495aaba51844a8a4
Cory <cameyer182@gmail.com>
2023-04-25 21:47:31 -0400

    added link to frontend github in readme

--- README.md
+++ README.md
@@ -21,5 +21,6 @@ Things you may want to cover:
 
 * Deployment instructions
 
-* ...
+* link to the frontend - https://github.com/cmeyerSE/f1_teams.git
+
 # f1_teams_api
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Team.destroy_all
+Driver.destroy_all
+
 
 fastcars = Team.create(name: "Fast Cars")
 bestdrivers = Team.create(name: "Best Drivers")
 
-Driver.create(name: "Danny Speed", description: "One of the fastest drivers alive", skill: "Speed Burst", team_id: 1)
+danny = Driver.create(name: "Danny Speed", description: "One of the fastest drivers alive", skill: "Speed Burst", team_id: fastcars.id)
+will = Driver.create(name: "Willam Turner", description: "Best corner racer!", skill: "Fast Corners", team_id: bestdrivers.id)
+billy = Driver.create(name: "Billy Bumper", description: "Watch out for this one", skill: "Bump and Go", team_id: bestdrivers.id)
