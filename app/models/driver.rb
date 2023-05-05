db7ed9d82bcd470eb11f7da26e8a886d0a078860
Cory <cameyer182@gmail.com>
2023-05-05 13:14:25 -0400

    added validations in models

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,9 @@
 class Driver < ApplicationRecord
     belongs_to :team
 
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :skill, presence: true
+
     scope :sort_by_teams, -> {order(team_id: :desc)}
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,7 @@
 class Team < ApplicationRecord
     has_many :drivers
 
+    validates :name, presence: true
+
     scope :latest_team, -> {order(created_at: :desc)}
 end
